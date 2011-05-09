41902715bbe292933a7183c27d3cbca5c47f961c
bry <bry@bryanbibat.net>
2011-05-09 18:43:28 +0800

    initial commit (p7)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlingnenaRails3::Application.config.secret_token = '25c1a9496dadf742f609378e3a3bd35a508fbd1b7248cf272b4760f5374308f8902afd1ceea2399d94fec695b0cb0d29a926ab37e176d1789abeb6fd4d05b60d'
