6524b27abf654529cb3717569568783858bca510
bry <bry@bryanbibat.net>
2011-05-09 18:59:12 +0800

    Modify views for remarks (p28-29)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110509104821) do
+ActiveRecord::Schema.define(:version => 20110509105114) do
 
   create_table "debts", :force => true do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(:version => 20110509104821) do
     t.decimal  "amount"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "remarks"
   end
 
 end
