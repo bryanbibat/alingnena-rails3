3c9c23b9d466a4b128cee35cdcc1dc51039e8b84
bry <bry@bryanbibat.net>
2011-05-09 20:43:31 +0800

    Start coding Products Index (p44)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 AlingnenaRails3::Application.routes.draw do
   resources :debts
 
+  match 'products' => 'products#index', :via => :get 
   match 'products/:id' => 'products#show', :via => :get
 
 end
