43070a4b87d538ade9cbc8fff3a395e4e6553255
bry <bry@bryanbibat.net>
2011-05-10 17:37:54 +0800

    Ajax move code to RJS (p134-135)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -31,17 +31,7 @@ class MessagesController < ApplicationController
       if @message.save
         format.html { redirect_to(messages_path, :notice => 'Message was successfully created.') }
         format.xml  { render :xml => @message, :status => :created, :location => @message }
-        format.js do
-          render :update do |page|
-            page.replace_html :message_list,
-              :partial => Message.all(:order => "created_at DESC")
-            page.replace_html :notice, 'Message was successfully created.'
-            page.delay(20) do
-              page.visual_effect :fade, 'notice'
-            end
-            page[:new_message].reset
-          end
-        end
+        format.js 
       else
         @messages = Message.all(:order => "created_at DESC")
         format.html { render :action => "index" }
