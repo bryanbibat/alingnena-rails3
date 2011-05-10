a3708867b22c10bf9e0f01a195f8eaa3022f4379
bry <bry@bryanbibat.net>
2011-05-10 15:42:36 +0800

    Invoice helper (p90)

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,2 +1,9 @@
 module InvoicesHelper
+  def display_purchase(invoice)
+    unless invoice.purchase.nil?
+      link_to invoice.purchase.description, invoice.purchase
+    else
+      "(no Purchase set)"
+    end
+  end
 end
