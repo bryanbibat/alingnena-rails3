83b7c4216b8e03d2186383d4f7005b625f6d34c2
bry <bry@bryanbibat.net>
2011-05-10 16:31:02 +0800

    HABTM example (p101)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110510080542) do
+ActiveRecord::Schema.define(:version => 20110510082720) do
 
   create_table "debts", :force => true do |t|
     t.string   "name"
@@ -35,6 +35,11 @@ ActiveRecord::Schema.define(:version => 20110510080542) do
     t.integer  "stock"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "products_purchases", :id => false, :force => true do |t|
+    t.integer "product_id"
+    t.integer "purchase_id"
   end
 
   create_table "purchases", :force => true do |t|
