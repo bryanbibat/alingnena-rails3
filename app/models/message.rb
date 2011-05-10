6c186b5400b84b8986c8fc63360f57e0babd34ba
bry <bry@bryanbibat.net>
2011-05-10 17:32:28 +0800

    Ajax form_for :remote (p131-132)

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,10 @@
 class Message < ActiveRecord::Base
+  validates_presence_of :author, :message
+  before_validation :set_anonymous
+  
+  private
+
+    def set_anonymous
+      self.author = "Anonymous" if author.blank?
+    end
 end
